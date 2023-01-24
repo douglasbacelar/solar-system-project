24d6e6bb87ea169426dca6f9e3153c1a5f25af15
Douglas Bacelar <bacelardouglas@outlook.com>
2023-01-24 14:57:40 -0300

    Requisito 4

--- src/components/SolarSystem.js
+++ src/components/SolarSystem.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import Title from './Title';
 
 class SolarSystem extends React.Component {
   render() {
+    const { headline } = this.props;
     return (
-      <div data-testid="solar-system"> </div>
+      <div data-testid="solar-system">
+        <Title
+          headline="Planetas"
+        />
+      </div>
     );
   }
 }

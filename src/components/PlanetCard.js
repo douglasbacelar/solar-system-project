d0c26148f15276fb805c999f09a609b2c8ae1e2e
Douglas Bacelar <bacelardouglas@outlook.com>
2023-01-24 15:31:42 -0300

    Requisito 5

--- src/components/PlanetCard.js
+++ src/components/PlanetCard.js
@@ -6,7 +6,7 @@ class PlanetCard extends React.Component {
     return (
       <div data-testid="planet-card">
         <p data-testid="planet-name">{ planetName }</p>
-        <img src={ planetImage } alt=`Planet ${ planetName }` ></img>
+        <img src={ planetImage } alt={`Planeta ${ planetName}`} ></img>
       </div>
     );
   }
